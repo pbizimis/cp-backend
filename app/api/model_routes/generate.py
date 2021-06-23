d32cdb5b8b068184a92fc22112f124eacc6e2054
Philip <bizimisphilip@gmail.com>
2021-06-23 17:12:34 +0200

    Add Auth0 protected route and logic

--- app/api/model_routes/generate.py
+++ app/api/model_routes/generate.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Security
+from fastapi_auth0 import Auth0User
+from app.api.auth import auth
 
 router = APIRouter()
 
 @router.get("/random")
 def generate_image():
     return {"message": "Generated!"}
+
+@router.get("/protected")
+def generate_image_protected(user: Auth0User = Security(auth.get_user, scopes=["use:all"])):
+    return {"message": "Protected Generated!"}
