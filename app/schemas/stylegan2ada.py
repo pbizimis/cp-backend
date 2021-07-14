245180687ab1d1242cd8c8ff1b9af3618c32ab3f
Philip <bizimisphilip@gmail.com>
2021-07-15 00:59:10 +0200

    Change name value

--- app/schemas/stylegan2ada.py
+++ app/schemas/stylegan2ada.py
@@ -14,7 +14,7 @@ generation_method = StyleGanMethod(
     name="Generate",
     description="Generate random images or from a certain seed.",
     method_options=(
-        Dropdown(place=1, name="Models", options=stylegan2_ada_models.models, default=0),
+        Dropdown(place=1, name="Model", options=stylegan2_ada_models.models, default=0),
         Slider(place=2, name="Truncation", max=2, min=-2, step=0.1, default=1)
     )
 )
@@ -23,7 +23,7 @@ stylemix_method = StyleGanMethod(
     name="StyleMix",
     description="Style mix different images.",
     method_options=(
-        Dropdown(place=1, name="Models", options=stylegan2_ada_models.models, default=0),
+        Dropdown(place=1, name="Model", options=stylegan2_ada_models.models, default=0),
         Slider(place=2, name="Images", max=6, min=0, step=1, default=3)
     )
 )
