dc1e730dc27452afa58cff532b4c755e2f7b9e0f
Philip <bizimisphilip@gmail.com>
2021-07-20 16:43:30 +0200

    Add stylegan user schema class

--- app/api/routes/stylegan2ada.py
+++ app/api/routes/stylegan2ada.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Depends, Security
 from fastapi_auth0 import Auth0User
 from app.core.auth0 import auth
-from app.schemas.stylegan2ada import StyleMix, Generation, generation_method, stylemix_method
+from app.schemas.stylegan2ada import StyleMix, Generation, generation_method, stylemix_method, StyleGan2ADA
 from app.schemas.stylegan_models import stylegan2_ada_models
+from app.schemas.stylegan_user import StyleGanUser
+from app.db.mongodb import get_db
+from motor.motor_asyncio import AsyncIOMotorClient
 
 router = APIRouter()
 
@@ -15,8 +18,15 @@ def style_mix_images(model: StyleMix, user: Auth0User = Security(auth.get_user, 
     return model
 
 @router.post("/generate")
-def generate_image(model: Generation, user: Auth0User = Security(auth.get_user, scopes=["use:all"])):
-    return model
+async def generate_image(generation_options: Generation, db: AsyncIOMotorClient = Depends(get_db),  user: Auth0User = Security(auth.get_user, scopes=["use:all"])):
+
+    stylegan_user = StyleGanUser(user, generation_options, StyleGan2ADA, db)
+
+    image_id = stylegan_user.generate_stylegan_image()
+    
+    await stylegan_user.save_stylegan_image()
+
+    return {"message": image_id}
 
 @router.get("/methods")
 def generate_image(generation_method: dict = Depends(generation_method), stylemix_method: dict = Depends(stylemix_method), user: Auth0User = Security(auth.get_user, scopes=["use:all"])):
---
+++ app/schemas/stylegan_user.py
@@ -0,0 +1,27 @@
+from fastapi_auth0 import Auth0User
+from app.schemas.stylegan_models import StyleGanModel
+from typing import Type
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.db.mongodb import save_image
+from app.schemas.mongodb import Image, User
+from app.db.google_cloud_storage import upload_blob
+
+class StyleGanUser:
+
+    def __init__(self, user: Auth0User, stylegan_method_options: dict, stylegan_class: Type[StyleGanModel], db: AsyncIOMotorClient):
+        self.user = user
+        self.stylegan_method_options = stylegan_method_options
+        self.stylegan_class = stylegan_class
+        self.db = db
+        self.stylegan_model = self._load_stylegan_model()
+
+    def _load_stylegan_model(self) -> StyleGanModel:
+        return self.stylegan_class(self.stylegan_method_options.model, self.stylegan_method_options)
+
+    def generate_stylegan_image(self) -> None:
+        self.image_blob = self.stylegan_model.generate()
+    
+    async def save_stylegan_image(self) -> None:
+        self.image_id = upload_blob("stylegan-images", self.image_blob)
+        image_data = Image(url=self.image_id, auth0_id=self.user.id, stylegan_data=self.stylegan_method_options)
+        await save_image(self.db, image_data)
