422abe6671c935ee5c5403a7f5b1c9106e31e521
Philip <bizimisphilip@gmail.com>
2021-07-27 19:16:18 +0200

    Change auth0 api audience

--- app/core/auth0.py
+++ app/core/auth0.py
@@ -1,3 +1,3 @@
 from fastapi_auth0 import Auth0
 
-auth = Auth0(domain="capstone-test.eu.auth0.com", api_audience="https://testapi-service-mdvcgw37oq-ew.a.run.app", scopes={"use:all": "Use all models"})
+auth = Auth0(domain="capstone-test.eu.auth0.com", api_audience="https://api.webdesigan.com", scopes={"use:all": "Use all models"})
