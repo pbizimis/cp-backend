8aa820978c623bcc8551014f2eabb91dcd78312b
Philip <bizimisphilip@gmail.com>
2021-07-29 13:44:15 +0200

    Add new image storage url

--- app/core/config.py
+++ app/core/config.py
@@ -2,4 +2,4 @@ API_NAME = "CP_API"
 VERSION = "0.0.1"
 API_PREFIX = "/api/v1"
 DEBUG = True
-IMAGE_STORAGE_BASE_URL = "https://storage.googleapis.com/stylegan-images/"
+IMAGE_STORAGE_BASE_URL = "https://images.webdesigan.com/"
