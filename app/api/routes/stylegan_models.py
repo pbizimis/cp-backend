d77be64974f332ff2f63bb859a2cb4d2eca4987d
Philip <bizimisphilip@gmail.com>
2021-08-06 19:39:35 +0200

    Change the returned data structure

--- app/api/routes/stylegan_models.py
+++ app/api/routes/stylegan_models.py
@@ -7,4 +7,6 @@ router = APIRouter()
 
 @router.get("/models")
 def read_stylegan_models(stylegan2ada_models: list = Depends(stylegan2_ada_models), user: Auth0User = Security(auth.get_user, scopes=["use:all"])):
-    return {"stylegan_models": {"version": "StyleGan2ADA", "models": stylegan2ada_models}}
+    return {"stylegan_models": [
+        {"version": "StyleGan2ADA", "models": stylegan2ada_models}
+        ]}
