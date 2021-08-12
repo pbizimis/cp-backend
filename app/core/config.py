9739de84c292ca4822f2eb2398851fa4053669f1
Philip <bizimisphilip@gmail.com>
2021-08-12 02:15:51 +0200

    Add env variable support for the debug state of the application

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 API_NAME = "CP_API"
 VERSION = "0.0.1"
 API_PREFIX = "/api/v1"
-DEBUG = True
+DEBUG = bool(os.getenv("FASTAPI_DEBUG"))
 IMAGE_STORAGE_BASE_URL = "https://images.webdesigan.com/"
 # Auth0
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
